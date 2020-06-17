11558a880bbe99fcd8e225518baeab68341ac306
javikalsan <jcalzadosanchez@gmail.com>
2020-06-17 15:39:03 +0200

    Add go version manager

--- os/src.packages.sh
+++ os/src.packages.sh
@@ -6,3 +6,5 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 # node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
+# go version manager
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
