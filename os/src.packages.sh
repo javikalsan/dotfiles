110d268b7c20ea6a6c5e3f0e86913495b943bb8d
javikalsan <jcalzadosanchez@gmail.com>
2021-04-25 21:39:14 +0200

    Change node and golang version manager

--- os/src.packages.sh
+++ os/src.packages.sh
@@ -4,10 +4,10 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 # node version manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -L https://git.io/n-install | bash
 
 # go version manager
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+curl -sSL https://git.io/g-install | sh -s
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
