007ddcdb39276f8d9b574bc7182741236ba3b817
javikalsan <jcalzadosanchez@gmail.com>
2021-06-21 12:01:42 +0200

    Fix indentation

--- psql/functions/search_field.sql
+++ psql/functions/search_field.sql
@@ -9,9 +9,9 @@ BEGIN
 
     SELECT t.table_name
     FROM information_schema.tables t
-	INNER JOIN information_schema.columns c ON c.table_name = t.table_name
+    INNER JOIN information_schema.columns c ON c.table_name = t.table_name
     WHERE c.column_name = search_field_name
-	AND c.table_schema = t.table_schema
+    AND c.table_schema = t.table_schema
     AND t.table_schema NOT IN ('information_schema', 'pg_catalog')
     AND t.table_type = 'BASE TABLE'
     ORDER BY t.table_schema;
